6d4221f0d7480f7803482e5a19ae90962af7e121
Alok Pradhan <alokpra@gmail.com>
2020-06-06 23:12:47 -0700

    API functional.

--- app/controllers/book_search_controller.rb
+++ app/controllers/book_search_controller.rb
@@ -2,16 +2,14 @@ class BookSearchController < ApplicationController
 
   def index 
     key = Rails.application.secrets.goodreads_key
-    secret = Rails.application.secrets.goodreads_secret
-
-    @books = Hash.from_xml(HTTParty.get("https://www.goodreads.com/search/index.xml?key=#{key}&q=#{search_params[:term]}"))
+    @books = HTTParty.get("https://www.goodreads.com/search/index.xml?key=#{key}&q=#{search_params[:term]}").parsed_response
     render json: @books, status: :ok
   end
 
   private
 
   def search_params
-    params.require(:search).permit(:term)
+    params.permit(:term)
   end
 
 end
